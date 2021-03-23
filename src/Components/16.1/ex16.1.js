59502045c17fca3b1cfc6956e257a5f9eae5c1c7
eyal <eyalilan8@gmail.com>
2021-03-23 11:45:34 +0200

    add Router

---
+++ src/Components/16.1/ex16.1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ex16 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
